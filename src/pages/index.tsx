8adefb3411e54e2c7975b47ed2173bc33fa13421
Angelia Cuaca <angelia@phoria.com.au>
2024-01-10 18:58:53 +1100

    Handle screen with smaller screen size instead of redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from "react"
 import { NextPage } from "next"
 import Resume from "@/components/home/Resume"
+import Print from "./print"
 
 const Home: NextPage = () => {
   const [page, setPage] = useState(<div />)
-  useEffect(() => {
-    if (window.screen.width < 600) {
-      window.location.href = "/print"
+
+  const handleScreenWidth = () => {
+    if (window.screen.width < 1080) {
+      setPage(<Print />)
     } else {
       setPage(<Resume />)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("load", handleScreenWidth)
+    window.addEventListener("resize", handleScreenWidth)
+
+    return () => {
+      window.removeEventListener("load", handleScreenWidth)
+      window.removeEventListener("resize", handleScreenWidth)
     }
   }, [])
 
